1f5f239c8301827e57dd96517e9c8dcd5eb4b83d
Viet Tran <tranhuyviet@gmail.com>
2019-10-03 22:15:18 +0300

    setup started basic css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
