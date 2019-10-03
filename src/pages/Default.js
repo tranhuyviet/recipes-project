9005476396b75610442d1b22e81bfed897b2aacf
Viet Tran <tranhuyviet@gmail.com>
2019-10-04 00:09:01 +0300

    finish home page, 404 page, navbar component and header component

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
+import Header from "../components/Header";
+
+import { Link } from "react-router-dom";
+
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h1>Error Page</h1>
-      </div>
+      <Header title="404" styleClass="header-error">
+        <h2 className="text-light text-uppercase">page not found</h2>
+
+        <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+          back to home page
+        </Link>
+      </Header>
     );
   }
 }
